601c61a70ec66804cf699a26a6ae646b62ec34c3
Jiri Spac <capajj@gmail.com>
2015-01-28 23:26:22 +0000

    this was intended code organization

--- server.js
+++ server.js
@@ -3,44 +3,11 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 var config = require('config');
-var users = require('./data/default-users.json');
-var _ = require('lodash');
 
-var hat = require('hat');
-
-/**
- * holds active tokens
- * @type {Array<String>}
- */
-var loggedInTokens = [];
+var api = require('./src/api')(app);
 
 app.listen(config.port).on('listening', function() {
 	console.log("server started on ", config.port);
-});
-
-app.post('/auth', function(req, res) {
-	var input = req.body;
-	input.name = input.name.toLowerCase();
-	var user =_.find(users, input);	//we expect that our names are stored lowercased
-	//TODO store in mongo
-	if (user) {
-		var token = hat();
-		loggedInTokens.push(token);
-		res.status(201).send(token)
-	} else {
-		res.send(401);
-	}
-});
-
-app.delete('/auth/:token', function(req, res){
-	var index = loggedInTokens.indexOf(req.token);
-	if (index !== -1) {
-		loggedInTokens.splice(index, 1);
-		res.send(200);
-	} else {
-		res.send(404);
-	}
-
 });
 
 module.exports = app;
--- src/api.js
+++ src/api.js
@@ -1,3 +1,36 @@
+var hat = require('hat');
+var _ = require('lodash');
+var users = require('../data/default-users.json');
+
 /**
- * Created by capaj on 28/01/15.
+ * holds active tokens
+ * @type {Array<String>}
  */
+var loggedInTokens = [];
+
+module.exports = function(app) {
+	app.post('/auth', function(req, res) {
+		var input = req.body;
+		input.name = input.name.toLowerCase();
+		var user =_.find(users, input);	//we expect that our names are stored lowercased
+		//TODO store in mongo
+		if (user) {
+			var token = hat();
+			loggedInTokens.push(token);
+			res.status(201).send(token)
+		} else {
+			res.send(401);
+		}
+	});
+
+	app.delete('/auth/:token', function(req, res){
+		var index = loggedInTokens.indexOf(req.token);
+		if (index !== -1) {
+			loggedInTokens.splice(index, 1);
+			res.send(200);
+		} else {
+			res.send(404);
+		}
+
+	});
+};
