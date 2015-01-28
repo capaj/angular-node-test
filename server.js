bae73d218fad9ade83733ad8d2a9e48a20d86d3a
Jiri Spac <capajj@gmail.com>
2015-01-28 23:14:06 +0000

    first test passing

--- server.js
+++ server.js
@@ -1,4 +1,44 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+app.use(bodyParser.json());
+var config = require('config');
+var users = require('./data/default-users.json');
+var _ = require('lodash');
+
+var hat = require('hat');
+
+/**
+ * holds active tokens
+ * @type {Array<String>}
+ */
+var loggedInTokens = [];
+
+app.listen(config.port).on('listening', function() {
+	console.log("server started on ", config.port);
+});
+
+app.post('/auth', function(req, res) {
+	var user =_.find(users, req.body);
+	//TODO store in mongo
+	if (user) {
+		var token = hat();
+		loggedInTokens.push(token);
+		res.status(201).send(token)
+	} else {
+		res.send(401);
+	}
+});
+
+app.delete('/auth/:token', function(req, res){
+	var index = loggedInTokens.indexOf(req.token);
+	if (index !== -1) {
+		loggedInTokens.splice(index, 1);
+		res.send(200);
+	} else {
+		res.send(404);
+	}
+
+});
 
 module.exports = app;
--- test/api.spec.js
+++ test/api.spec.js
@@ -1,44 +1,60 @@
-var socket = require('socket.io-client')('http://localhost:8050');
+//var socket = require('socket.io-client')('http://localhost:8050');
 var server = require('../server');
 var request = require('supertest-as-promised');
 var users = require('../data/default-users.json');
 var Promise = require('bluebird');
+require('chai').should();
 
 /**
  * @param {Object} user
  * @returns {Promise}
  * @param {Number} [resCode] if not provided, expects 200
  */
-function makeReq(user, resCode) {
+function makeAuthReq(user, resCode) {
 	return request(server)
 		.post('/auth')
 		.send(user)
-		.expect(200 || resCode)
+		.expect(201 || resCode)
 }
 
 describe('simple auth api.spec', function(){
 	it('should authenticate for known users', function(done){
-		var allTestedPromises = users.map(makeReq);
-		Promise.all(allTestedPromises).then(done);
+		this.timeout = 10000;
+		var allTestedPromises = users.map(makeAuthReq);
+		Promise.all(allTestedPromises).then(function(responses) {
+			done();
+		});
 	});
 
 	it('should authenticate for a known user with mismatched casing', function(done){
 		var user = users[0];
 		var allUpperCaseName = user.name.toUpperCase();
-		makeReq({name: allUpperCaseName, password: user.password}).then(done);
+		makeAuthReq({name: allUpperCaseName, password: user.password}).then(done);
 	});
 
 	it('should NOT authenticate for an unknown user', function(done){
 		var user = {name: 'john.smith', password: ''};
-		makeReq(user, 401).then(done);
+		makeAuthReq(user, 401).then(done);
 	});
 
 	it('should NOT authenticate for known user with wrong password', function(done){
 		var user = {name: users[0].name, password: ''};
-		makeReq(user, 401).then(done);
+		makeAuthReq(user, 401).then(done);
+	});
+	
+	it('should be able to logout a user who logged in', function(){
+		request(server)
+			.delete('/auth/' + token)
+			.expect(200);
 	});
 
-	describe('recording of attempts', function(){
+	it('should 404 when a user attempts to logout who is not logged in', function(){
+		request(server)
+			.delete('/auth/' + token)
+			.expect(404);
+	});
+
+	describe.skip('recording of attempts', function(){
 		it('should record any authentication attempt in mongo', function(){
 
 		});
@@ -49,8 +65,11 @@ describe('simple auth api.spec', function(){
 				c++;
 			});
 
-			Promise.all(users.map(makeReq)).then(function() {
-				c.should.equal()
+
+
+			Promise.all(users.map(makeAuthReq)).then(function() {
+				c.should.equal(users.length + 1);
+				done();
 			});
 
 		});
